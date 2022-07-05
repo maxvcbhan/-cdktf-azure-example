0e9615bf3e0b77572449b4b45bd6e129379b10d8
Vorabhol Chuaybhan <Vorabhol.Chuaybhan@ibm.com>
2022-07-05 09:34:03 +0700

    Add cosmos db

--- main.ts
+++ main.ts
@@ -2,8 +2,10 @@ import {Construct} from "constructs";
 import {App, AzurermBackend, TerraformStack} from "cdktf";
 import {
 	AzurermProvider,
+	CosmosdbAccount, CosmosdbMongoCollection, CosmosdbMongoDatabase,
 	StorageAccount,
-	StorageBlob, StorageContainer
+	StorageBlob,
+	StorageContainer
 } from "@cdktf/provider-azurerm";
 import {
 	StorageAccountConfig
@@ -15,8 +17,9 @@ config();
 class MyStack extends TerraformStack {
 	constructor(scope: Construct, name: string) {
 		super(scope, name);
-		const storageAccountName = "cloudportalcdktf";
-
+		const storageAccountName = "cloudportalac";
+		const resourceGroupName = "testLB";
+		const baseNameApplication = "cloud-portal";
 		new AzurermBackend(this, {
 			storageAccountName: "tfstate2762",
 			resourceGroupName: "tfstate",
@@ -30,6 +33,7 @@ class MyStack extends TerraformStack {
 		});
 
 		new AzurermProvider(this, "azure", {
+			subscriptionId: process.env.SUBSCRIPTION_ID || "",
 			features: {
 				apiManagement: {
 					purgeSoftDeleteOnDestroy: true,
@@ -74,6 +78,7 @@ class MyStack extends TerraformStack {
 			},
 
 		})
+
 		const storageConfig: StorageAccountConfig = {
 			accountTier: "Standard",
 			resourceGroupName: "testLB",
@@ -83,17 +88,51 @@ class MyStack extends TerraformStack {
 		}
 		const sa = new StorageAccount(this, "storage-account", storageConfig);
 		const container: StorageContainer = new StorageContainer(this, "container-name", {
-			name: "cloud-portal-document",
+			name: `${baseNameApplication}-document`,
 			containerAccessType: "container",
 			storageAccountName: storageConfig.name
 		});
-		new StorageBlob(this, "terraform-state", {
-			accessTier: "Hot",
+		new StorageBlob(this, `${baseNameApplication}-blob`, {
+			accessTier: "Cool",
 			storageAccountName: sa.name,
-			name: "terraform state",
+			name: "document",
 			storageContainerName: container.name,
 			type: "Block"
 		});
+		const cosmosdbAccount = new CosmosdbAccount(this, `${baseNameApplication}-cosmos-account`, {
+			name: `${baseNameApplication}-cosmosdb-account`,
+			location: "Southeast Asia",
+			resourceGroupName: resourceGroupName,
+			offerType: "Standard",
+			kind: "MongoDB",
+			enableAutomaticFailover: false,
+			mongoServerVersion: "4.2",
+			enableFreeTier: true,
+			consistencyPolicy: {
+				consistencyLevel: "Session"
+			},
+			geoLocation: [{
+				failoverPriority: 0,
+				location: "East Asia"
+			}]
+
+		});
+
+		const cosmosdbMongoDatabase = new CosmosdbMongoDatabase(this, `${baseNameApplication}-db`, {
+			resourceGroupName: resourceGroupName,
+			accountName: cosmosdbAccount.name,
+			name: `${baseNameApplication}-db`
+		})
+		const collectionNames = ["questionnaires", "files", "users", "assessment"];
+		collectionNames.map((collectionName) => {
+			return new CosmosdbMongoCollection(this, `${baseNameApplication}-${collectionName}`, {
+				name: collectionName,
+				index: [{keys: ["_id"]}],
+				resourceGroupName: resourceGroupName,
+				accountName: cosmosdbAccount.name,
+				databaseName: cosmosdbMongoDatabase.name
+			})
+		})
 	}
 }
 
