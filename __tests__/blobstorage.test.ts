a2bbe136d1a4e90151926ea6440eb2e1e1164383
Vorabhol Chuaybhan <Vorabhol.Chuaybhan@ibm.com>
2022-08-09 09:15:17 +0700

    Add blob storage snapshort test

--- __tests__/blobstorage.test.ts
+++ __tests__/blobstorage.test.ts
@@ -11,7 +11,7 @@ describe('My CDKTF Application', () => {
 
 	describe('Block storage', () => {
 		const stack = Testing.synthScope((scope) => {
-			new BlobStorageConfig(scope, 'my-app-under-test', 'mock-storage-account-name','mock-base-app',
+			new BlobStorageConfig(scope, 'my-app-under-test', 'mock-storage-account-name', 'mock-base-app',
 				new ResourceGroup(scope, 'acc', {
 					location: 'mock-location', name: 'mock-name',
 				}));
@@ -35,5 +35,55 @@ describe('My CDKTF Application', () => {
 			});
 		});
 
+	});
+
+
+});
+describe('Unit testing using snapshots', () => {
+	it('Tests the blob storage snapshot', () => {
+		expect(
+			Testing.synthScope((stack) => {
+				new BlobStorageConfig(stack, 'my-app-under-test', 'mock-storage-account-name', 'mock-base-app',
+					new ResourceGroup(stack, 'acc', {
+						location: 'mock-location', name: 'mock-name',
+					}));
+
+			})).toMatchInlineSnapshot(`
+"{
+  \\"resource\\": {
+    \\"azurerm_resource_group\\": {
+      \\"acc\\": {
+        \\"location\\": \\"mock-location\\",
+        \\"name\\": \\"mock-name\\"
+      }
+    },
+    \\"azurerm_storage_account\\": {
+      \\"my-app-under-test_mock-base-app-storage-account_A4E8D618\\": {
+        \\"account_replication_type\\": \\"LRS\\",
+        \\"account_tier\\": \\"Standard\\",
+        \\"location\\": \\"\${azurerm_resource_group.acc.location}\\",
+        \\"name\\": \\"mock-storage-account-name\\",
+        \\"resource_group_name\\": \\"\${azurerm_resource_group.acc.name}\\"
+      }
+    },
+    \\"azurerm_storage_blob\\": {
+      \\"my-app-under-test_mock-base-app-blob_B52E41CB\\": {
+        \\"access_tier\\": \\"Cool\\",
+        \\"name\\": \\"document\\",
+        \\"storage_account_name\\": \\"\${azurerm_storage_account.my-app-under-test_mock-base-app-storage-account_A4E8D618.name}\\",
+        \\"storage_container_name\\": \\"\${azurerm_storage_container.my-app-under-test_mock-base-app-blob-container_E605FBD8.name}\\",
+        \\"type\\": \\"Block\\"
+      }
+    },
+    \\"azurerm_storage_container\\": {
+      \\"my-app-under-test_mock-base-app-blob-container_E605FBD8\\": {
+        \\"container_access_type\\": \\"container\\",
+        \\"name\\": \\"mock-base-app-container\\",
+        \\"storage_account_name\\": \\"\${azurerm_storage_account.my-app-under-test_mock-base-app-storage-account_A4E8D618.name}\\"
+      }
+    }
+  }
+}"
+`);
 	});
 });
